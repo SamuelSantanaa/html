6a3ff8a6b8c7419eb2a1aaaa524718434bc5db2d
SamuelSantanaa <samurthz@gmail.com>
2024-11-19 20:38:49 +0000

    19/11/24

--- proyecto1/Proyecto2.js
+++ proyecto1/Proyecto2.js
@@ -1,5 +1,3 @@
-// Selecciona el botón por su ID y agrega un evento click
-document.getElementById("mostrarFechaHora").onclick = function() {
-    // Muestra la fecha y hora actuales en el elemento con ID "demo"
+{
     document.getElementById("demo").innerHTML = new Date();
 };
